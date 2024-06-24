3a19ef1f8dc3722fe04b6cf242c0ef6ba90bc30a
ThanhDT <dotrungthanh2300@gmail.com>
2024-06-24 15:49:20 +0700

    Rotate plane to original rotation in given time

--- Prototype 1/Assets/Challenge 1/Scripts/PlaneController.cs
+++ Prototype 1/Assets/Challenge 1/Scripts/PlaneController.cs
@@ -6,18 +6,46 @@ public class PlayerControllerX : MonoBehaviour
 {
     public float speed;
     public float rotationSpeed;
-    private float verticalInput;
+    private int verticalInput;
+    private Rigidbody rb;
+    private float time;
+    public float duration;
+    private Quaternion rotationOnKeyUp;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+        time = 0;
+        rotationOnKeyUp = Quaternion.identity;
+    }
+
+    private void Update() {
+        // get the user's vertical input
+        verticalInput = (int) Input.GetAxisRaw("Vertical");
+
+        if (verticalInput == 0 && time == 0) {
+            rotationOnKeyUp = transform.rotation;
+        }
+
+        time += Time.deltaTime;
+        time *= verticalInput == 0 ? 1 : 0;
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        // get the user's vertical input
-        verticalInput = Input.GetAxis("Vertical");
-
         // move the plane forward at a constant rate
-        transform.Translate(transform.forward * speed);
+        rb.MovePosition(transform.position + transform.forward * speed);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(transform.right * rotationSpeed * Time.deltaTime * -verticalInput);
+        rb.MoveRotation(transform.rotation * Quaternion.Euler(transform.right * rotationSpeed * Time.fixedDeltaTime * -verticalInput));
+        
+        if (verticalInput == 0) Rotate();
     }
+
+    private void Rotate() {
+        Quaternion rot = Quaternion.Lerp(rotationOnKeyUp, Quaternion.identity, time / duration);
+        rb.MoveRotation(rot);
+    }
+
+    
 }
